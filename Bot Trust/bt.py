b0556b95048c8903756d64354fd399214f94800b
unknown <ct057688@057A0157978.Global.Lloydstsb.Com>
2012-12-11 20:11:31 +0000

    Added incomplete move robot def to bot trust

--- Bot Trust/bt.py
+++ Bot Trust/bt.py
@@ -7,8 +7,8 @@ def BuildStack(ip):
     # ip is a string with position and color in pairs.  no seperators.
     # Example ip: B4O1B5B6B2O2
     
-	x = []                                        # x is the list to be returned.  Initialise
-	
+    x = []                                        # x is the list to be returned.  Initialise
+    
     # Output is a list of buttons,in same order as they appear in the list.
     for i in range(0, len(ip) / 2):
         x.append(Button(ip[0], ip[1]))            # create a new button and append to list.
@@ -16,4 +16,31 @@ def BuildStack(ip):
     
     # Return x
     return x
-        
+
+	
+def MoveRobot(robot, stack):
+	# Parsed the global move stack and move the robot one step closer to the next (update object)
+	# target position.  If there are no more buttons to visit, or if the robot is
+	# already at the next target position, then do nothing.
+	# Return : updated robot object.
+	
+	targetPosition = 0						# initiliase the target position of the robot to move.
+	
+	for element in stack:
+		if ( robot.color == element.color ):
+			targetPosition = element.position
+			break
+	
+	if ( targetPosition == 0 ):
+		# robot has no buttons to visit
+		pass
+	else:
+		if ( targetPosition < robot.position ):
+			robot.position = robot.position - 1
+		
+		if (targetPosition > robot.position):
+				robot.position = robot.position + 1
+			
+		if (targetPosition == robot.position):
+			# robot already @ target position, pass.
+			pass
