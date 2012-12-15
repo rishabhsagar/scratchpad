04a1bfec276dfa4663fa79783d3726c3800008f6
unknown <ct057688@057A0157978.Global.Lloydstsb.Com>
2012-12-15 11:04:29 +0000

    updates bot trust program

--- Bot Trust/bt.py
+++ Bot Trust/bt.py
@@ -3,6 +3,11 @@ class Button:
         self.color = color
         self.position = position
 
+class Robot:
+	def __init__(self, color, position):
+		self.color = color
+		self.position = position
+		
 def BuildStack(ip):
     # ip is a string with position and color in pairs.  no seperators.
     # Example ip: B4O1B5B6B2O2
@@ -11,8 +16,8 @@ def BuildStack(ip):
     
     # Output is a list of buttons,in same order as they appear in the list.
     for i in range(0, len(ip) / 2):
-        x.append(Button(ip[0], ip[1]))            # create a new button and append to list.
-        ip = ip[2:]                               # trim the ip to remove the last two bits
+        x.append(Button(ip[0], int(ip[1])))            # create a new button and append to list.
+        ip = ip[2:]                                    # trim the ip to remove the last two bits
     
     # Return x
     return x
@@ -28,7 +33,7 @@ def MoveRobot(robot, stack):
 	
 	for element in stack:
 		if ( robot.color == element.color ):
-			targetPosition = element.position
+			targetPosition = int(element.position)
 			break
 	
 	if ( targetPosition == 0 ):
@@ -44,3 +49,31 @@ def MoveRobot(robot, stack):
 		if (targetPosition == robot.position):
 			# robot already @ target position, pass.
 			pass
+
+def PressButton(robots, stack):
+	# Receives an array of robots, and a stack.
+	# Analysed the position of the robots against the next button to be pressed (top of the stack).
+	# If robot of correct color is @ the same position as the top of the stack, press that button i.e - pop top of the stack.
+	tick = 0								# Tells if a button was pressed, if yes tick is set to 1.  Caller learns that button was pressed.
+	
+	for robot in robots:
+		if ( robot.color == stack[0].color and robot.position == stack[0].position ):
+			print "Robot @ " + str(robot.position) + " press button in " + stack[0].color + " position."
+			stack.pop(0)
+			tick = 1
+			
+	return tick
+	
+# Main programm to run the simulation
+tick = 0				# Simulates clock ticks.
+ip = 'O2B1B2O4'			# Simulated input, in the real problem, this will come from file.
+stk = BuildStack(ip)	# build stack from parsed input.
+
+O = Robot('O', 1)
+B = Robot('B', 1)		# Build the robots
+
+while (len(stk) > 0 ):
+	MoveRobot(B,stk)
+	MoveRobot(O, stk)
+	tick = tick + 1 + PressButton([B,O], stk)
+	print "Tick"
