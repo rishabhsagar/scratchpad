b1610c1880cdc3f0b045dc3cfe36f55094cc0cfa
Rishabh Sagar <Rishabh.Sagar@gmail.com>
2013-05-14 09:01:33 +0100

    Solved problem 35, added eulerutils.py

---
+++ euler/eulerutils.py
@@ -0,0 +1,42 @@
+import math
+
+def isPrime(num):
+  '''@Summary: Checks if a given number is prime.
+     @Returns: Returns True if Prime, else False.
+     @Attention: Returns False for 1 and negative numbers.
+  '''
+
+  if ( num <= 1 ): return False 
+  if num == 2 : return True
+  if ( num % 2 == 0): return False  # Quick check for even numbers.
+
+  upperLimit = int(math.sqrt(num) + 1) # Upper limit is the max value to be checked.
+  
+  for x in range(2, upperLimit):
+    if ( (num % x) == 0 ): return False
+
+  return True # If execution comes to this line, then num in Prime.
+
+def primesUpto(num):
+    '''
+    @Summary: Generates primes upto num.
+    @Return: Returns prime numbers upto num 
+    '''
+
+    nums = range(2,num) #Generate a list of all nums
+    nums = filter(lambda x: x % 2 !=0, nums)    #cuts problem by half.
+    primes = [2]   #List of primes, initialised with a few primes.
+
+    while len(nums) != 0:
+        nums = filter(lambda x: x % primes[-1] != 0, nums)
+        if len(nums) > 0: primes.append(nums[0])
+
+    return primes
+
+def iprimesUpto(limit):
+    is_prime = [True] * limit
+    for n in range(2, limit):
+        if is_prime[n]:
+            yield n
+            for i in range(n*n, limit, n): # start at ``n`` squared
+                is_prime[i] = False
--- euler/pr35.py
+++ euler/pr35.py
@@ -1,19 +1,4 @@
-import math
-
-def isPrime(num):
-  '''@Summary: Checks if a given number is prime.
-     @Returns: Returns True if Prime, else False.
-     @Attention: Returns False for 1 and negative numbers.
-  '''
-
-  if ( num <= 1 ): return False 
-
-  upperLimit = int(math.sqrt(num) + 1) # Upper limit is the max value to be checked.
-  
-  for x in range(2, upperLimit):
-    if ( (num % x) == 0 ): return False
-
-  return True # If execution comes to this line, then num in Prime.
+from eulerutils import *
 
 def rotate(num):
   '''@summary: Rotates a given number's digits.
@@ -24,13 +9,12 @@ def rotate(num):
   num = str(num) # Convert the number to string type
   for i in range(0, len(num)):
     num = num[1:] + num[0]
-://rishabhsagar@bitbucket.org/rishabhsagar/scratchpad.git
     rot_nums.append(int(num))
 
   return rot_nums
 
 if __name__ == "__main__":
-  prime_list = primeUpto(1000000) # Create a list of prime numbers upto 1M.
+  prime_list = iprimesUpto(1000000) # Create a list of prime numbers upto 1M.
   count = 0
   cir_prime = []
 
