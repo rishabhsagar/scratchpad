c591b5abf7a8b00e3c0d78a80a4adc6692c9e799
Rishabh Sagar <Rishabh.Sagar@gmail.com>
2013-05-14 07:40:12 +0100

    removed primesUpto(incorrect)

--- euler/pr35.py
+++ euler/pr35.py
@@ -24,37 +24,10 @@ def rotate(num):
   num = str(num) # Convert the number to string type
   for i in range(0, len(num)):
     num = num[1:] + num[0]
+://rishabhsagar@bitbucket.org/rishabhsagar/scratchpad.git
     rot_nums.append(int(num))
 
   return rot_nums
-
-def primeUpto(n):
-  #Start with a known primeList
-  primeList = [2,3,5,7,11,13]
-  invEnd = 0
-  while invEnd < n:
-
-    #Start from last known prime, and end at 2ice that prime
-    invStart = primeList[len(primeList) - 1]
-  
-    if pow(invStart,2) < n:
-      invEnd = pow(invStart,2)
-    else:
-      invEnd = n
-
-    
-    #Build required range
-    sv = range(invStart, invEnd+1)
-
-    #For each known prime, execute sieve of Eratosthenes (remove multiples of known primes)
-    for prime in primeList:
-      sv = filter(lambda x: (x % prime) != 0, sv)
-    
-    #Numbers surviving in the list are primes, add these to known list of primes
-    primeList = primeList + sv
-
-    return primeList
-
 
 if __name__ == "__main__":
   prime_list = primeUpto(1000000) # Create a list of prime numbers upto 1M.
@@ -71,6 +44,6 @@ if __name__ == "__main__":
     if ( False not in map(isPrime, rot_prime)):
       count = count + 1
       print str(prime) + " is a circular prime."
-      cir_prime = cir_prime + rot_prime # Adding all rotations to cir_prime list.  These need not be checked again.
+      cir_prime = cir_prime + rot_prime # Adding all rotations to cir_prime list.
 
   print "---Found " + str(count) + " circular primes.---"
